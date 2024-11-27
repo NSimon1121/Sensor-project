fc71777518d39d4dcd4c6760843c3d374907bd99
NSimon1121 <tup76485@temple.edu>
2024-11-27 14:06:02 -0500

    Initial Commit

---
+++ Sensor project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sensor project
+//
+//  Created by Nicholas Simon on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sensor projectTests/Sensor_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sensor_projectTests.swift
+//  Sensor projectTests
+//
+//  Created by Nicholas Simon on 11/27/24.
+//
+
+import Testing
+@testable import Sensor_project
+
+struct Sensor_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
